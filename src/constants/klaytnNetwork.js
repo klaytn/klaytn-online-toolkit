2ed0ac10b181321dc67df1b721b946f7498104e9
Yeonju-Kim <krust@krustui-MacBookPro-88.local>
2022-09-02 18:24:24 +0900

    Delete scss files

--- src/constants/klaytnNetwork.js
+++ src/constants/klaytnNetwork.js
@@ -8,3 +8,9 @@ export const networkLinks = {
         "finder": "https://www.klaytnfinder.io/tx/"
     },
 }
+
+export const KIPLinks = {
+    "KIP-7": "https://kips.klaytn.foundation/KIPs/kip-7",
+    "KIP-17": "https://kips.klaytn.foundation/KIPs/kip-17",
+    "KIP-37": "https://kips.klaytn.foundation/KIPs/kip-37",
+}
