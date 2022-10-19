94111b7273a33f9f6f599dc33fd89d6c3325737a
Lim SangHyun <skqksh@gmail.com>
2022-10-19 11:43:04 +0900

    Refactoring to CRA

---
+++ src/views/ABIEncoder.jsx
@@ -0,0 +1,143 @@
+import React, { Component } from 'react'
+import { Button, Card, CardHeader, CardBody, Row, Col, Label } from 'reactstrap'
+import Caver from 'caver-js'
+import { networkLinks } from '../constants/klaytnNetwork'
+import Column from '../components/Column'
+
+const caver = new Caver(
+  new Caver.providers.HttpProvider(networkLinks['mainnet']['rpc'])
+)
+
+const INPUT_ERROR_MSG = '[ERROR] PLEASE ENTER THE CORRECT FORMAT OF INPUTS!'
+
+class ABIEncoder extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      result: '',
+      argumentTypes: '',
+      argumentValues: '',
+      copy: '',
+    }
+  }
+
+  handleInputChange = (e) => {
+    const { name, value } = e.target
+    this.setState({
+      [name]: value,
+    })
+  }
+
+  copyToClipboard = () => {
+    const el = this.copy
+    el.select()
+    document.execCommand('copy')
+  }
+
+  encodeABI = async () => {
+    try {
+      const { argumentTypes, argumentValues } = this.state
+      const types = JSON.parse('[' + argumentTypes + ']')
+      const values = JSON.parse('[' + argumentValues + ']')
+      const res = await caver.abi.encodeParameters(types, values)
+      if (res) {
+        this.setState({ result: res })
+      } else {
+        this.setState({ result: '' })
+      }
+    } catch (err) {
+      this.setState({ result: INPUT_ERROR_MSG })
+    }
+  }
+
+  render() {
+    const { argumentTypes, argumentValues, result } = this.state
+    return (
+      <div>
+        <Column>
+          <Card>
+            <CardHeader>
+              <h3 className="title">ABI Encoder</h3>
+              <p style={{ color: '#6c757d' }}>
+                The tool is designed to encode Solidity ABI data.
+              </p>
+            </CardHeader>
+            <CardBody>
+              <Row>
+                <Col>
+                  <Label>Argument Types</Label>
+                  <p style={{ color: '#6c757d' }}>
+                    Enter the comma-separated value types.
+                  </p>
+                  <textarea
+                    className="form-control"
+                    value={argumentTypes}
+                    onChange={this.handleInputChange}
+                    placeholder='Argument Types ( Example1 : "bool", "address" || Example2 : "uint8[]", "string" )'
+                    style={{
+                      height: '120px',
+                      backgroundColor: '#adb5bd',
+                      color: 'black',
+                    }}
+                    name="argumentTypes"
+                  />
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Label>Argument Values</Label>
+                  <p style={{ color: '#6c757d' }}>
+                    Enter the comma-separated values to match the number of
+                    types shown above.<br></br>
+                  </p>
+                  <textarea
+                    className="form-control"
+                    value={argumentValues}
+                    onChange={this.handleInputChange}
+                    placeholder='Argument Values (Example1 : true, "0x77656c636f6d6520746f20657468657265756d2e" || Example2 : [34, 255], "Hello!%")'
+                    style={{
+                      height: '120px',
+                      backgroundColor: '#adb5bd',
+                      color: 'black',
+                    }}
+                    name="argumentValues"
+                  />
+                </Col>
+              </Row>
+              <Row>
+                <Col md="4">
+                  <Button onClick={(e) => this.encodeABI()}>ENCODE</Button>
+                </Col>
+              </Row>
+              {result && result !== INPUT_ERROR_MSG ? (
+                <Row>
+                  <Col>
+                    <Label>Result</Label>
+                    <textarea
+                      className="form-control"
+                      ref={(textarea) => (this.copy = textarea)}
+                      style={{
+                        height: '120px',
+                        backgroundColor: '#adb5bd',
+                        color: 'black',
+                      }}
+                      value={result}
+                      readOnly
+                    />
+                    <Button onClick={() => this.copyToClipboard()}>
+                      Copy To Clipboard
+                    </Button>
+                  </Col>
+                </Row>
+              ) : (
+                <p style={{ color: '#c221a9' }}> {result} </p>
+              )}
+            </CardBody>
+          </Card>
+        </Column>
+      </div>
+    )
+  }
+}
+
+export default ABIEncoder
---
+++ src/views/txHashDecoder.jsx
@@ -0,0 +1,170 @@
+import React, { Component } from 'react'
+import { Button, Card, CardHeader, CardBody, Row, Col, Label } from 'reactstrap'
+import Caver from 'caver-js'
+import { networkLinks } from '../constants/klaytnNetwork'
+import Column from '../components/Column'
+
+let caver = new Caver(
+  new Caver.providers.HttpProvider(networkLinks['mainnet']['rpc'])
+)
+
+const INPUT_ERROR_MSG =
+  '[ERROR] PLEASE ENTER THE CORRECT VALUE OF TRANSACTION HASH!'
+
+class TxHashDecoder extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      network: 'mainnet',
+      result: '',
+      rawResult: '',
+      txHash: '',
+      tx: '',
+      rawTx: '',
+    }
+  }
+
+  handleInputChange = (e) => {
+    const { name, value } = e.target
+    this.setState({
+      [name]: value,
+    })
+  }
+
+  handleNetworkChange = (e) => {
+    caver = new Caver(
+      new Caver.providers.HttpProvider(networkLinks[e.target.value]['rpc'])
+    )
+    this.setState({
+      network: e.target.value,
+    })
+  }
+
+  decodeTxHash = async () => {
+    const { txHash } = this.state
+    try {
+      const res = await caver.transaction.getTransactionByHash(txHash)
+      if (res) {
+        this.setState({
+          result: JSON.stringify(res, null, 2),
+          rawResult: res.getRawTransaction(),
+        })
+      } else {
+        this.setState({ result: '' })
+      }
+    } catch (err) {
+      this.setState({ result: INPUT_ERROR_MSG })
+    }
+  }
+
+  copyTxToClipboard = () => {
+    const el = this.tx
+    el.select()
+    document.execCommand('copy')
+  }
+
+  copyRawTxToClipboard = () => {
+    const el = this.rawTx
+    el.select()
+    document.execCommand('copy')
+  }
+
+  render() {
+    const { txHash, result, rawResult } = this.state
+    return (
+      <div>
+        <Column>
+          <Card>
+            <CardHeader>
+              <h3 className="title">TxHash Decoder</h3>
+              <p style={{ color: '#6c757d' }}>
+                On this page, you can query a transaction by transaction hash
+                from the Klaytn network to get a caver transaction instance.
+              </p>
+              <Row>
+                <Col md="4">
+                  <select
+                    onChange={(e) => this.handleNetworkChange(e)}
+                    className="form-control"
+                  >
+                    <option value="mainnet"> Mainnet</option>
+                    <option value="testnet"> Testnet</option>
+                  </select>
+                </Col>
+              </Row>
+            </CardHeader>
+            <CardBody>
+              <Row>
+                <Col>
+                  <Label>Transaction Hash</Label>
+                  <p style={{ color: '#6c757d' }}>
+                    Enter the Transaction Hash.
+                  </p>
+                  <textarea
+                    className="form-control"
+                    value={txHash}
+                    onChange={this.handleInputChange}
+                    placeholder="TxHash ( Example : 0x272272d25387cd8b0d3bf842d0d9fa2dee7c014ae66c3fd7a53865453d9bc7cc )"
+                    style={{
+                      height: '80px',
+                      backgroundColor: '#adb5bd',
+                      color: 'black',
+                    }}
+                    name="txHash"
+                  />
+                </Col>
+              </Row>
+              <Row>
+                <Col md="4">
+                  <Button onClick={(e) => this.decodeTxHash()}>DECODE</Button>
+                </Col>
+              </Row>
+              {result && result !== INPUT_ERROR_MSG ? (
+                <Col>
+                  <Row>
+                    <Label>Transaction</Label>
+                    <textarea
+                      className="form-control"
+                      ref={(textarea) => (this.tx = textarea)}
+                      style={{
+                        height: '600px',
+                        backgroundColor: '#adb5bd',
+                        color: 'black',
+                      }}
+                      value={result}
+                      readOnly
+                    />
+                    <Button onClick={() => this.copyTxToClipboard()}>
+                      Copy To Clipboard
+                    </Button>
+                  </Row>
+                  <Row>
+                    <Label>Raw Transaction(RLP-encoded Transaction)</Label>
+                    <textarea
+                      className="form-control"
+                      ref={(textarea) => (this.rawTx = textarea)}
+                      style={{
+                        height: '300px',
+                        backgroundColor: '#adb5bd',
+                        color: 'black',
+                      }}
+                      value={rawResult}
+                      readOnly
+                    />
+                    <Button onClick={() => this.copyRawTxToClipboard()}>
+                      Copy To Clipboard
+                    </Button>
+                  </Row>
+                </Col>
+              ) : (
+                <p style={{ color: '#c221a9' }}> {result} </p>
+              )}
+            </CardBody>
+          </Card>
+        </Column>
+      </div>
+    )
+  }
+}
+
+export default TxHashDecoder
