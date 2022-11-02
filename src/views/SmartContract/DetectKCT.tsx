4f8a99cde34ab2b8392487e8b45ea817366a4c40
Edwin <edwin.best@krustuniverse.com>
2022-11-02 16:04:10 +0900

    Update detectKCT page

--- src/views/SmartContract/DetectKCT.tsx
+++ src/views/SmartContract/DetectKCT.tsx
@@ -13,20 +13,17 @@ import {
   FormSelect,
   FormInput,
   Button,
-  FormTextarea,
-  FormRadio,
-  ResultForm,
   CardSection,
-  CodeBlock,
-  CardExample,
+  ResultForm,
 } from 'components'
+
+import { ResultFormType } from 'types'
 
 type NetworkType = 'mainnet' | 'testnet'
 
 const DetectKCT = (): ReactElement => {
   const [contractAddress, setContractAddress] = useState('')
-  const [result, setResult] = useState('')
-  const [success, setSuccess] = useState(false)
+  const [result, setResult] = useState<ResultFormType>()
   const [network, setNetwork] = useState<NetworkType>('mainnet')
 
   const caver = useMemo(
@@ -38,34 +35,49 @@ const DetectKCT = (): ReactElement => {
     try {
       const kip7 = await caver.kct.kip7.detectInterface(contractAddress)
       if (kip7.IKIP7) {
-        setSuccess(true)
-        setResult('KIP-7')
+        setResult({
+          success: true,
+          value: 'KIP-7',
+        })
+        return
       }
-    } catch (err: any) {
-      setSuccess(false)
-      setResult(err.toString())
+    } catch (err) {
+      setResult({
+        success: false,
+        message: _.toString(err),
+      })
     }
 
     try {
       const kip17 = await caver.kct.kip17.detectInterface(contractAddress)
       if (kip17.IKIP17) {
-        setSuccess(true)
-        setResult('KIP-17')
+        setResult({
+          success: true,
+          value: 'KIP-17',
+        })
+        return
       }
-    } catch (err: any) {
-      setSuccess(false)
-      setResult(err.toString())
+    } catch (err) {
+      setResult({
+        success: false,
+        message: _.toString(err),
+      })
     }
 
     try {
       const kip37 = await caver.kct.kip37.detectInterface(contractAddress)
       if (kip37.IKIP37) {
-        setSuccess(true)
-        setResult('KIP-37')
+        setResult({
+          success: true,
+          value: 'KIP-37',
+        })
+        return
       }
-    } catch (err: any) {
-      setSuccess(false)
-      setResult(err.toString())
+    } catch (err) {
+      setResult({
+        success: false,
+        message: _.toString(err),
+      })
     }
   }
 
@@ -75,7 +87,7 @@ const DetectKCT = (): ReactElement => {
         <CardHeader>
           <h3 className="title">Klaytn Compatible Token(KCT) Detection</h3>
           <Text>
-            <a href="https://kips.klaytn.foundation/KIPs/kip-7">
+            <a href="https://kips.klaytn.foundation/token">
               {' '}
               Klaytn Compatible Token
             </a>{' '}
@@ -107,14 +119,7 @@ const DetectKCT = (): ReactElement => {
             <br />
             <Button onClick={detectKCT}>Check</Button>
           </CardSection>
-          {success ? (
-            <Text>
-              This smart contract implements{' '}
-              <a href={URLMAP.kip[result]}>{result} Token Standard</a>.
-            </Text>
-          ) : (
-            <Text> {result} </Text>
-          )}
+          <ResultForm title={'Result'} result={result} />
         </CardBody>
       </Card>
     </Column>
